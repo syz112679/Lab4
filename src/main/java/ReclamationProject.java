9c847b0faccac92708487cf446aa0b85eb2c748a
syz112679 <syz112679@gmail.com>
2017-09-27 10:48:27 -0500

    Update ReclamationProject.java
    I don't why I cannot commit and pushup on eclipse

--- src/main/java/ReclamationProject.java
+++ src/main/java/ReclamationProject.java
@@ -7,20 +7,44 @@
  * 4. Add comments and Javadoc comments where needed
  * 5. Remove unnecessary comments as appropriate
  */
-
-public class ReclamationProject
-{
-    static String doit(String a,String b){
-        if (a.length() > b.length()){
-            String c = a; // TODO: set c to a
-            a=b; b=c;}
-        String r = (a.equals(b)) ? "" : ""; // I love the ternary operator!
+/**
+ * I don't know what to write for javadoc.
+ * @param
+ * @return
+ * @see
+ */
+public class ReclamationProject {
+    /**
+     * I don't know what to write for javadoc.
+     * @param originalA is string
+     * @param originalB is string
+     * @return nothing
+     * @see
+     */
+    static String doit(final String originalA, final String originalB) {
+        String a = originalA, b = originalB;
+        if (a.length() > b.length()) {
+            String c = a; // set c to a
+            a = b;
+            b = c;
+        }
+        String r = "";
         /*
          * For loop with i
          */
-        for (int i = 0; i < a.length(); i++) { for (int j = a.length() - i; j > 0; j--) {
-                for (int k = 0; k < b.length()- j; k++) {
-                    r = (a.regionMatches(i, b, k, j) && j >r.length()) ? a.substring(i,i + j) : r; // Do it!
-                        }} // Ah yeah
-        } return r; }
+        for (int i = 0; i < a.length(); i++) {
+            for (int j = a.length() - i; j > 0; j--) {
+                for (int k = 0; k < b.length() - j; k++) {
+                    boolean firstCondition = a.regionMatches(i, b, k, j);
+                    if (firstCondition) {
+                        if (j > r.length()) {
+                            a.substring(i, i + j);
+                        }
+                    }
+                    // r = ( firstCondition && j > r.length() ) ? a.substring(i, i + j) : r; Do it!
+                }
+            } // Ah yeah
+        }
+        return r;
+        }
 }
